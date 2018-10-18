b762c0a29e5dfb7b0e56251ca02d66be2ce91c2f
Costin Grigore <costin000@hotmail.es>
2018-10-18 13:58:36 +0100

    Still working on the main function

--- MainFile.c
+++ MainFile.c
@@ -1,17 +1,36 @@
 
+#include <stdio.h>
 
-typedef struct row
-{
-	int array[10];
-} Row;
-
-int parent[10] = {0,0,0,0,1,0,0,0};
+int parent[10] = {0,1,0,0,1,0,0,0};
 int child[1];
 
 int checkValues(int row){
-	if (parent[0]==0)
+	int node1;
+	int node2;
+	int node3;
+	for (int i = 0, i < 10, i = i++)
 	{
-		child[0]=0;
+
+		node1=parent[i-1];
+		node2=parent[i];
+		node3=parent[i+1];
+		if (node2==parent[0]){
+			if (node2==0 && node3==0)
+			{
+				child[i]=0;
+			}
+			else if (node2==0 && node3==1)
+			{
+				child[i]=1;
+			}
+		}
+		if(node2=!parent[0])
+		{
+			if (node1==1 && node2==1 && node3==1){
+				child[i]=0;
+			}
+		}
+
 	}
 
 }
@@ -21,4 +40,5 @@ int main ()
 
  checkValues(parent);
  printf("%d\n",child[0]);
+ printf("%d\n",child[1]);
 }
