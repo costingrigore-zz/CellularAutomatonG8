b81c521a4d50a4439c3ffc456b20ecc274f63bc8
Costin Grigore <costin000@hotmail.es>
2018-10-18 13:03:02 +0100

    A few new changes

--- Cellular Automaton.c
+++ Cellular Automaton.c
@@ -8,7 +8,17 @@ typedef struct row
 Row parent;
 Row child;
 
+int checkValues(Row row){
+	if (parent->array[0]==0)
+	{
+		*child->array[0]=0;
+	}
+
+}
+
 int main ()
 {
  parent->array={0,0,0,0,1,0,0,0}
+ checkValues(parent);
+ prinf(child->array)
 }
