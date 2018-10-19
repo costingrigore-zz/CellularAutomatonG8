29a125a53e9de13253044d3b83c4403fcccf9a19
RJM1999 <33182930+RJM1999@users.noreply.github.com>
2018-10-19 16:33:13 +0100

    New Commit

--- Cellular Automaton.c
+++ Cellular Automaton.c
@@ -1,24 +1,77 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-typedef struct row
+int parent[30] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+
+int child[30];
+
+
+void checkValues(int *parent, int *child)
 {
-	int array[10];
-} Row;
+    
+    for(int i = 0; i < 30; i++)
+    {
+        printf("%d",parent[i]);
+    }
+    printf("\n");
+    
+    for(int i = 0; i < 30; i++)
+    {
+        int left = parent[i-1];
+        int middle = parent[i];
+        int right = parent[i+1];
+        
+        if( left == 0 && middle == 0 && right == 0)
+        {
+            child[i] = 0;
+        }
+        else if (left == 0 && middle == 0 && right == 1)
+        {
+            child[i] = 1;
+        }
+        else if (left == 0 && middle == 1 && right == 0)
+        {
+            child[i] = 1;
+            
+        }
+        else if (left == 0 && middle == 1 && right == 1)
+        {
+            child[i] = 1;
+        }
+        else if (left == 1 && middle == 0 && right == 0)
+        {
+            child[i] = 1;
+        }
+        else if (left == 1 && middle == 0 && right == 1)
+        {
+            child[i] = 0;
+            
+        }
+        else if (left == 1 && middle == 1 && right == 0)
+        {
+            child[i] = 0;
+        }
+        else if (left == 1 && middle == 1 && right == 1)
+        {
+            child[i] = 0;
+        }
+    }
+    
+    for(int i = 0; i < 30; i++)
+    {
+        printf("%d",child[i]);
+    }
+    printf("\n");
 
-int parent[10] = {0,0,0,0,1,0,0,0};
-int child[1];
-
-int checkValues(int row){
-	if (parent[0]==0)
-	{
-		child[0]=0;
-	}
-
+    parent = child;
+    
+    int newChild [30];
+    
+    child = newChild;
+    
 }
 
 int main ()
 {
-
- checkValues(parent);
- printf("%d\n",child[0]);
+    checkValues(parent, child);
 }
