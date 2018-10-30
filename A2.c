3da44d8480d2ee288f9246d8ad62c84f28fb8cb3
Costin Grigore <costin000@hotmail.es>
2018-10-30 13:12:49 +0000

    Convert from binary to decimal method added

--- A2.c
+++ A2.c
@@ -34,12 +34,10 @@ void checkValues()
 		for(int i = 0; i<30; i++)
 		{
 			parent[i] = child[i];
-		}
-			  
+		}	  
 	}
-	
-	
 }
+
 int rules(int left, int middle, int right)
 {
 	 if( left == 0 && middle == 0 && right == 0)
@@ -77,7 +75,38 @@ int rules(int left, int middle, int right)
 		    return 0;
 		}
 }
+
+//I based this method on this reference: https://www.sanfoundry.com/c-program-binary-number-into-decimal/
+int convert( int[] num)
+{
+
+    int decimal_val = 0, base = 1, rem;
+
+ 	int original = num;
+
+    while (num > 0)
+
+    {
+
+        rem = num % 10;
+
+        decimal_val = decimal_val + rem * base;
+
+        num = num / 10 ;
+
+        base = base * 2;
+
+    }
+
+    printf("The Binary number is = %d \n", original);
+
+    printf("Its decimal equivalent is = %d \n", decimal_val);
+
+    original=0;
+}
 int main ()
 {
     checkValues();
+    convert(101010100);
+    convert(0100);
 }
